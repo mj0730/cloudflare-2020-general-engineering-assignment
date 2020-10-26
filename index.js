483c7995e7d12db42da0dc1f9b8664db7f61ec9a
Michael Johnson <mrj0730@icloud.com>
2020-10-26 17:56:43 -0500

    add links with htmlrewriter

--- index.js
+++ index.js
@@ -23,10 +23,32 @@ async function handler(request) {
 async function handleRequest(request) {
     const r = new Router()
     r.get('/links', request => handler(request))
-    r.get('/.*', () =>
-        fetch('https://static-links-page.signalnerve.workers.dev')
-    )
+    r.get('/.*', () => addLinks())
 
     const resp = await r.route(request)
     return resp
+}
+
+async function addLinks() {
+    const html = await fetch(
+        'https://static-links-page.signalnerve.workers.dev'
+    )
+
+    return new HTMLRewriter()
+        .on('#links', new LinksTransformer(links))
+        .transform(html)
+}
+
+class LinksTransformer {
+    constructor(links) {
+        this.links = links
+    }
+
+    element(element) {
+        this.links.forEach(link =>
+            element.append(`<a href=${link.url}>${link.name}</a>`, {
+                html: true,
+            })
+        )
+    }
 }
